5059765cfa1957bf7ddc69bfedc473789c0b1afd
segara2410 <segara2410@gmail.com>
2020-05-07 04:27:15 +0700

    tidy up

--- cp.c
+++ cp.c
@@ -18,135 +18,135 @@ char* strcat(char *target, char *source)
 
 char *getFileName(char *s) 
 {
-	char *filename = s;
-	char *temp = s;
-	int i;
+  char *filename = s;
+  char *temp = s;
+  int i;
 
-	for (i = strlen(temp); i >= 0; i--) 
+  for (i = strlen(temp); i >= 0; i--) 
   {
-		if (temp[i] == '/') 
+    if (temp[i] == '/') 
     {
-			filename = &temp[i + 1];
-			break;
-		}
-	}
+      filename = &temp[i + 1];
+      break;
+    }
+  }
 
-	return filename;
+  return filename;
 }
 
 int isDirectory(char *s) 
 {
-	struct stat st;
+  struct stat st;
 
-	int fd = open(s, O_RDONLY);
-	fstat(fd, &st);
-	int is_directory;
+  int fd = open(s, O_RDONLY);
+  fstat(fd, &st);
+  int is_directory;
 
-	if (st.type == T_DIR) 
-		is_directory = 1;
-	else
-		is_directory = 0;
+  if (st.type == T_DIR) 
+    is_directory = 1;
+  else
+    is_directory = 0;
 
-	close(fd);
-	return is_directory;
+  close(fd);
+  return is_directory;
 }
 
 void copy_file(char *source, char *target) 
 {
 
-	char *newtarget = (char *) malloc(strlen(getFileName(source)) + strlen(target) + 2);
-	strcpy(newtarget, target);
+  char *newtarget = (char *) malloc(strlen(getFileName(source)) + strlen(target) + 2);
+  strcpy(newtarget, target);
 
-	if (isDirectory(target)) 
+  if (isDirectory(target)) 
   {
-		if (target[strlen(target) - 1] != '/') 
+    if (target[strlen(target) - 1] != '/') 
       strcat(newtarget, "/");
 
-		strcat(newtarget, getFileName(source));
-	} 
+    strcat(newtarget, getFileName(source));
+  } 
   else if (target[strlen(target) - 1] == '/') 
   {
-		printf(1, "mv: %s is not a directory\n", target);
-		return;
-	}
+    printf(1, "mv: %s is not a directory\n", target);
+    return;
+  }
 
-	int fs;
-	if ((fs = open(source, O_RDONLY)) < 0) 
+  int fs;
+  if ((fs = open(source, O_RDONLY)) < 0) 
   {
-		printf(1, "mv: cannot open %s\n", source);
-		return;
-	}
+    printf(1, "mv: cannot open %s\n", source);
+    return;
+  }
 
   int fd;
-	if ((fd = open(newtarget, O_CREATE | O_RDWR)) < 0) 
+  if ((fd = open(newtarget, O_CREATE | O_RDWR)) < 0) 
   {
-		printf(1, "mv: cannot open %s\n", target);
-		return;
-	}
+    printf(1, "mv: cannot open %s\n", target);
+    return;
+  }
 
-	int fb;
+  int fb;
   char buffer[512];
-	while ((fb = read(fs, buffer, sizeof(buffer))) > 0) 
-		write(fd, buffer, fb);
+  while ((fb = read(fs, buffer, sizeof(buffer))) > 0) 
+    write(fd, buffer, fb);
 
-	close(fs);
-	close(fd);
+  close(fs);
+  close(fd);
 }
 
 void copy_recurse(char *source, char *target) 
 {
   int fd;
-	if((fd = open(source, 0)) < 0)
+  if((fd = open(source, 0)) < 0)
   {
-		printf(2, "cp: cannot open %s\n", source);
-		return;
-	}
+    printf(2, "cp: cannot open %s\n", source);
+    return;
+  }
 
   char bufdir[512];
-	strcpy(bufdir, target);
-	if (target[strlen(target) - 1] != '/') 
+  strcpy(bufdir, target);
+  if (target[strlen(target) - 1] != '/') 
     strcat(bufdir, "/");
-	
+  
   strcat(bufdir, getFileName(source));
 
-	if (mkdir(bufdir) < 0) 
+  if (mkdir(bufdir) < 0) 
   {
-		mkdir(target);
-		strcpy(bufdir, target);
-	}
+    mkdir(target);
+    strcpy(bufdir, target);
+  }
 
-	char buffer[512];
-	strcpy(buffer, source);
+  char buffer[512];
+  strcpy(buffer, source);
 
   char *p;
-	p = buffer + strlen(buffer);
-	*p = '/';
-	p++;
+  p = buffer + strlen(buffer);
+  *p = '/';
+  p++;
 
-	struct dirent de;
-	while(read(fd, &de, sizeof(de)) == sizeof(de)) 
+  struct dirent de;
+  while(read(fd, &de, sizeof(de)) == sizeof(de)) 
   {
-		if(de.inum == 0 || !strcmp(de.name, ".") || !strcmp(de.name, ".."))
-			continue;
+    if(de.inum == 0 || !strcmp(de.name, ".") || !strcmp(de.name, ".."))
+      continue;
 
-		memmove(p, de.name, strlen(de.name));
-		p[strlen(de.name)] = 0;
+    memmove(p, de.name, strlen(de.name));
+    p[strlen(de.name)] = 0;
 
-		if (isDirectory(buffer))
-			copy_recurse(buffer, bufdir);
-		else
-			copy_file(buffer, bufdir);
-	}
+    if (isDirectory(buffer))
+      copy_recurse(buffer, bufdir);
+    else
+      copy_file(buffer, bufdir);
+  }
 
-	close(fd);
+  close(fd);
 }
 
 void copy_one(char *source, char *target) 
 {
-	if (isDirectory(source))
-		copy_recurse(source, target);
-	else
-		copy_file(source, target);
+  if (isDirectory(source))
+    copy_recurse(source, target);
+  else
+    copy_file(source, target);
 }
 
 void copy_all(char *target) 
@@ -157,35 +157,35 @@ void copy_all(char *target)
 int main(int argc, char *argv[]) 
 {
 
-	if (argc < 2) 
+  if (argc < 2) 
   {
-		printf(1, "help..\n");
-		exit();
-	} 
+    printf(1, "help..\n");
+    exit();
+  } 
   else if (argc < 3) 
   {
-		printf(1, "cp: missing destination file operand after %s\n", argv[1]);
-		exit();
-	}
+    printf(1, "cp: missing destination file operand after %s\n", argv[1]);
+    exit();
+  }
 
-	if (!strcmp(argv[1], "*"))
-		copy_all(argv[2]);
-	else if (strcmp(argv[1], "-r") == 0)
+  if (!strcmp(argv[1], "*"))
+    copy_all(argv[2]);
+  else if (strcmp(argv[1], "-r") == 0)
   {
     if (argc > 4 && !isDirectory(argv[argc - 1]))
     {
-		  printf(2, "cp: target '%s' is not a directory\n", argv[argc - 1]);
+      printf(2, "cp: target '%s' is not a directory\n", argv[argc - 1]);
       exit();
     }
 
     for (int i = 2; i < argc - 1; i++)
       copy_one(argv[i], argv[argc - 1]);
   }
-	else
+  else
   {
     if (argc > 3 && !isDirectory(argv[argc - 1]))
     {
-		  printf(2, "cp: target '%s' is not a directory\n", argv[argc - 1]);
+      printf(2, "cp: target '%s' is not a directory\n", argv[argc - 1]);
       exit();
     }
 
@@ -199,8 +199,8 @@ int main(int argc, char *argv[])
     }
 
     for (int i = 1; i < argc - 1; i++)
-		  copy_one(argv[i], argv[argc - 1]);
+      copy_one(argv[i], argv[argc - 1]);
   }
 
-	exit();
+  exit();
 }
--- mv.c
+++ mv.c
@@ -18,140 +18,140 @@ char* strcat(char *target, char *source)
 
 char *getFileName(char *s) 
 {
-	char *filename = s;
-	char *temp = s;
-	int i;
+  char *filename = s;
+  char *temp = s;
+  int i;
 
-	for (i = strlen(temp); i >= 0; i--) 
+  for (i = strlen(temp); i >= 0; i--) 
   {
-		if (temp[i] == '/') 
+    if (temp[i] == '/') 
     {
-			filename = &temp[i + 1];
-			break;
-		}
-	}
+      filename = &temp[i + 1];
+      break;
+    }
+  }
 
-	return filename;
+  return filename;
 }
 
 int isDirectory(char *s) 
 {
-	struct stat st;
+  struct stat st;
 
-	int fd = open(s, O_RDONLY);
-	fstat(fd, &st);
-	int is_directory;
+  int fd = open(s, O_RDONLY);
+  fstat(fd, &st);
+  int is_directory;
 
-	if (st.type == T_DIR) 
-		is_directory = 1;
-	else
-		is_directory = 0;
+  if (st.type == T_DIR) 
+    is_directory = 1;
+  else
+    is_directory = 0;
 
-	close(fd);
-	return is_directory;
+  close(fd);
+  return is_directory;
 }
 
 void move_file(char *source, char *target) 
 {
 
-	char *newtarget = (char *) malloc(strlen(getFileName(source)) + strlen(target) + 2);
-	strcpy(newtarget, target);
+  char *newtarget = (char *) malloc(strlen(getFileName(source)) + strlen(target) + 2);
+  strcpy(newtarget, target);
 
-	if (isDirectory(target)) 
+  if (isDirectory(target)) 
   {
-		if (target[strlen(target) - 1] != '/') 
+    if (target[strlen(target) - 1] != '/') 
       strcat(newtarget, "/");
 
-		strcat(newtarget, getFileName(source));
-	} 
+    strcat(newtarget, getFileName(source));
+  } 
   else if (target[strlen(target) - 1] == '/') 
   {
-		printf(1, "mv: %s is not a directory\n", target);
-		return;
-	}
+    printf(1, "mv: %s is not a directory\n", target);
+    return;
+  }
 
-	int fs;
-	if ((fs = open(source, O_RDONLY)) < 0) 
+  int fs;
+  if ((fs = open(source, O_RDONLY)) < 0) 
   {
-		printf(1, "mv: cannot open %s\n", source);
-		return;
-	}
+    printf(1, "mv: cannot open %s\n", source);
+    return;
+  }
 
   int fd;
-	if ((fd = open(newtarget, O_CREATE | O_RDWR)) < 0) 
+  if ((fd = open(newtarget, O_CREATE | O_RDWR)) < 0) 
   {
-		printf(1, "mv: cannot open %s\n", target);
-		return;
-	}
+    printf(1, "mv: cannot open %s\n", target);
+    return;
+  }
 
-	int fb;
+  int fb;
   char buffer[512];
-	while ((fb = read(fs, buffer, sizeof(buffer))) > 0) 
-		write(fd, buffer, fb);
+  while ((fb = read(fs, buffer, sizeof(buffer))) > 0) 
+    write(fd, buffer, fb);
 
-	close(fs);
-	close(fd);
+  close(fs);
+  close(fd);
 
-	unlink(source);
+  unlink(source);
 }
 
 void move_dir(char *source, char *target) 
 {
 
-	int fd;
-	if((fd = open(source, 0)) < 0)
+  int fd;
+  if((fd = open(source, 0)) < 0)
   {
-		printf(2, "mv: cannot open %s\n", source);
-		return;
-	}
+    printf(2, "mv: cannot open %s\n", source);
+    return;
+  }
 
   char bufdir[512];
-	strcpy(bufdir, target);
-	if (target[strlen(target)-1] != '/') 
+  strcpy(bufdir, target);
+  if (target[strlen(target)-1] != '/') 
     strcat(bufdir, "/");
-	
+  
   strcat(bufdir, getFileName(source));
 
-	if (mkdir(bufdir) < 0) 
+  if (mkdir(bufdir) < 0) 
   {
-		mkdir(target);
-		strcpy(bufdir, target);
-	}
+    mkdir(target);
+    strcpy(bufdir, target);
+  }
 
-	char buffer[512];
-	strcpy(buffer, source);
+  char buffer[512];
+  strcpy(buffer, source);
 
   char *p;
-	p = buffer + strlen(buffer);
-	*p = '/';
-	p++;
+  p = buffer + strlen(buffer);
+  *p = '/';
+  p++;
 
-	struct dirent de;
-	while(read(fd, &de, sizeof(de)) == sizeof(de)) 
+  struct dirent de;
+  while(read(fd, &de, sizeof(de)) == sizeof(de)) 
   {
-		if(de.inum == 0 || !strcmp(de.name, ".") || !strcmp(de.name, ".."))
-			continue;
+    if(de.inum == 0 || !strcmp(de.name, ".") || !strcmp(de.name, ".."))
+      continue;
 
-		memmove(p, de.name, strlen(de.name));
-		p[strlen(de.name)] = 0;
+    memmove(p, de.name, strlen(de.name));
+    p[strlen(de.name)] = 0;
 
-		if (isDirectory(buffer))
-			move_dir(buffer, bufdir);
-		else
-			move_file(buffer, bufdir);
-	}
+    if (isDirectory(buffer))
+      move_dir(buffer, bufdir);
+    else
+      move_file(buffer, bufdir);
+  }
 
-	close(fd);
+  close(fd);
 
-	unlink(source);
+  unlink(source);
 }
 
 void move_one(char *source, char *target) 
 {
-	if (isDirectory(source))
-		move_dir(source, target);
-	else
-		move_file(source, target);
+  if (isDirectory(source))
+    move_dir(source, target);
+  else
+    move_file(source, target);
 }
 
 void move_all(char *target) 
@@ -160,30 +160,30 @@ void move_all(char *target)
 }
 
 int main(int argc, char *argv[]) {
-	if (argc < 2) 
+  if (argc < 2) 
   {
-		printf(1, "mv: missing file operand\n");
-		exit();
-	} 
+    printf(1, "mv: missing file operand\n");
+    exit();
+  } 
   else if (argc < 3) 
   {
-		printf(1, "mv: missing destination file operand after '%s'\n", argv[1]);
-		exit();
-	}
+    printf(1, "mv: missing destination file operand after '%s'\n", argv[1]);
+    exit();
+  }
 
-	if (!strcmp(argv[1], "*"))
-		move_all(argv[2]);
-	else
+  if (!strcmp(argv[1], "*"))
+    move_all(argv[2]);
+  else
   {
     if (argc > 3 && !isDirectory(argv[argc - 1]))
     {
-		  printf(2, "mv: target '%s' is not a directory\n", argv[argc - 1]);
+      printf(2, "mv: target '%s' is not a directory\n", argv[argc - 1]);
       exit();
     }
 
     for (int i = 1; i < argc - 1; i++)
-		  move_one(argv[i], argv[argc - 1]);
+      move_one(argv[i], argv[argc - 1]);
   }
 
-	exit();
+  exit();
 }
